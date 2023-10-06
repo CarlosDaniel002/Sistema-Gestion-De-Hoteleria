097d36c66a91efbb594659bdea9830e1a4330a15
Yohann0409 <145950939+Yohann0409@users.noreply.github.com>
2023-10-06 00:43:07 -0400

    Autentificación de usuarios al momento de iniciar sesion

--- Negocio/User/LoginFunctions.cs
+++ Negocio/User/LoginFunctions.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data.Entidades;
+using Data.UserData;
+using System.Globalization;
 
 namespace Negocio.User
 {
-    internal class LoginFunctions
+    public class LoginFunctions
     {
+        public static string LoginUser(string nombreUsuario, string contraseña)
+        {
+            // Buscar al usuario por nombre de usuario en la base de datos
+            Usuario usuario = LoginDB.GetUserByUserName(nombreUsuario);
+
+            if (usuario == null)
+            {
+                return "Usuario no encontrado";
+            }
+
+            // Obtener el salting almacenado para el usuario
+            byte[] salting = LoginDB.GetSaltingForUser(usuario.IdUsuario);
+
+            // Verificar si la contraseña proporcionada coincide con la almacenada en la base de datos
+            string hashContraseña = GetPassword.GenerateHash(contraseña, salting);
+
+            if (hashContraseña != usuario.Contraseña)
+            {
+                return "Contraseña incorrecta";
+            }
+
+            // Generar y devolver un nuevo token de autenticación para el usuario
+            string token = Token.GetTokenForIdUser(usuario.IdUsuario);
+
+            return token;
+        }
     }
 }
